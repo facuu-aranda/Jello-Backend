a9cdbd07b18d6422c76089529f85b4b00d99fbd3
Facu Aranda <facu.aranda.grf@gmail.com>
2025-11-03 16:13:46 -0300

    search.controller upgrade

--- controllers/search.controller.ts
+++ controllers/search.controller.ts
@@ -1,45 +1,49 @@
 import { Request, Response } from 'express';
-import { Project } from '../models/Project.model';
+import { Project } from '../models/Project.model'; 
 import { User } from '../models/User.model';
 import { IJwtPayload } from '../middleware/auth.middleware';
 
-export const globalSearch = async (req: Request, res: Response) => {
+export const globalSearch = async (req: Request, res: Response) => { 
     try {
         const query = req.query.q as string;
         const type = req.query.type as "all" | "user" | "project";
 
-        if (!query) {
-            return res.status(400).json({ error: "Se requiere un término de búsqueda." });
+        if (!query) { 
+            return res.status(400).json({ error: "Se requiere un término de búsqueda." }); 
         }
         const searchRegex = new RegExp(query, 'i');
         
         let results = [];
 
-        if (type === 'project' || type === 'all' || !type) {
-            const projects = await Project.find({ name: searchRegex }).limit(10);
+        if (type === 'project' || type === 'all' || !type) { 
+            const projects = await Project.find({ name: searchRegex }).limit(10); 
+            
             results.push(...projects.map(p => ({
                 type: 'project',
                 id: p._id,
                 name: p.name,
                 description: p.description,
-                avatar: p.projectImageUrl
+                avatar: p.projectImageUrl,
+                bannerImageUrl: p.bannerImageUrl 
             })));
         }
 
-        if (type === 'user' || type === 'all' || !type) {
+        if (type === 'user' || type === 'all' || !type) { 
             const users = await User.find({ name: searchRegex }).limit(10);
+            
             results.push(...users.map(u => ({
                 type: 'user',
                 id: u._id,
                 name: u.name,
                 description: u.jobTitle,
-                avatar: u.avatarUrl
-            })));
+                avatar: u.avatarUrl,
+                skills: u.skills,
+                bannerUrl: u.bannerUrl
+            }))); 
         }
         
-        res.json(results);
-
+        res.json(results); 
     } catch (error) {
-        res.status(500).json({ error: 'Error en el servidor', details: (error as Error).message });
+        res.status(500).json({ error: 'Error en el servidor', details: (error as Error).message }); 
     }
 };
